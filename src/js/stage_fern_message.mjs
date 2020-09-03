c5596dd93e0458babf2670e8e58adf360944799c
JackMorganNZ <jackmorgannz@gmail.com>
2020-09-03 16:12:11 +1200

    Add generation of word in fern leaves

--- src/js/stage_fern_message.mjs
+++ src/js/stage_fern_message.mjs
@@ -5,6 +5,55 @@ import { getSvg, changeStage, setSvgElementAnchor, addStylesToSvg } from './util
 // Import third party libraries
 import anime from 'animejs/lib/anime.es.js';
 
+// Leaf paths
+import leaf_back_1 from '../img/leaf-1-back-no-dots.svg';
+const LEAF_BACKS = [
+    leaf_back_1,
+]
+import leaf_front_1 from '../img/leaf-1-front.svg';
+const LEAF_FRONTS = [
+    leaf_front_1,
+]
+
+const POSSIBLE_WORDS = [
+    'toru',
+    'rima',
+    'whitu',
+    'waru',
+    'tekau',
+    // Current program doesn't support words with 'ng' or 'wh'.
+    // 'whā',
+    // Possibly too easy
+    // 'iwa',
+];
+
+const DECIMAL_DICTIONARY = {
+    ' ': 0,
+    'a': 1,
+    'ā': 2,
+    'e': 3,
+    'ē': 4,
+    'h': 5,
+    'i': 6,
+    'ī': 7,
+    'k': 8,
+    'm': 9,
+    'n': 10,
+    'ng': 11,
+    'o': 12,
+    'ō': 13,
+    'p': 14,
+    'r': 15,
+    't': 16,
+    'u': 17,
+    'ū': 18,
+    'w': 19,
+    'wh': 20,
+}
+
+var message_word = window.sessionStorage.getItem('fern-message-word');
+
+
 function start() {
     $('#stage-fern-message').removeClass('hidden');
     if (DEBUG) {
@@ -19,8 +68,12 @@ function start() {
 function setup() {
     $('#fern-message-previous-stage').on('click', { level_id: 'fern-interactive' }, end);
     $('#fern-message-next-stage').on('click', { level_id: 'unknown' }, end);
+
+    // Get SVG
     var svg = getSvg('fern-message-svg');
     addStylesToSvg(svg);
+
+    // Setup moss
     var moss = [svg.querySelector('#moss-1'), svg.querySelector('#moss-2')];
     moss.forEach(function (moss_element) {
         moss_element.classList.add('interactable');
@@ -34,6 +87,37 @@ function setup() {
             });
         });
     });
+
+    // Create word
+    if (!message_word) {
+        message_word = POSSIBLE_WORDS[Math.floor(Math.random() * POSSIBLE_WORDS.length)];
+        createWordWithBranches(message_word);
+    }
+}
+
+
+function createWordWithBranches(word) {
+    console.log(word);
+    var word_container = document.querySelector('#fern-message-container');
+    for (let i = 0; i < word.length; i++) {
+        var letter_container = document.createElement('div');
+        letter_container.classList.add('letter-container');
+        word_container.appendChild(letter_container);
+        let letter = word.charAt(i);
+        let letter_value = DECIMAL_DICTIONARY[letter];
+        let binary_string = letter_value.toString(2).padStart(5, '0');
+        for (let j = 0; j < binary_string.length; j++) {
+            if (binary_string[j] == '0') {
+                var image_path = LEAF_FRONTS[Math.floor(Math.random() * LEAF_FRONTS.length)];
+            } else {
+                var image_path = LEAF_BACKS[Math.floor(Math.random() * LEAF_BACKS.length)];
+            }
+            var digit_element = document.createElement('div');
+            digit_element.classList.add('digit-element');
+            digit_element.style.backgroundImage = 'url(' + image_path + ')';
+            letter_container.appendChild(digit_element);
+        }
+    }
 }
 
 

c8339b8b91caba6d9f41635b2cfc6935a7f10814
JackMorganNZ <jackmorgannz@gmail.com>
2020-09-17 16:49:51 +1200

    Add completion page content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+        },
+    },
+    variants: {},
+    plugins: [],
 }

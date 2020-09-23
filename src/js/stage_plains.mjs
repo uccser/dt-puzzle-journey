e748c1d7b0f0224f531755530d09f63f49d09fd8
JackMorganNZ <jackmorgannz@gmail.com>
2020-09-23 15:29:36 +1200

    Fix relative path

--- src/js/stage_plains.mjs
+++ src/js/stage_plains.mjs
@@ -551,7 +551,7 @@ function end() {
     $('#animation-blindfold').fadeIn(
         BLINDFOLD_FADE_DURATION,
         function () {
-            location.assign("/complete/index.html");
+            location.assign("./complete/index.html");
         }
     );
 }

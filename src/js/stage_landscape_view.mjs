24183057e812a961d8206e30994360496855c6f7
JackMorganNZ <jackmorgannz@gmail.com>
2020-09-29 21:28:30 +1300

    Update intro sequence

--- src/js/stage_landscape_view.mjs
+++ src/js/stage_landscape_view.mjs
@@ -5,127 +5,177 @@ import { getSvg, changeStage } from './utilities.mjs';
 // Import third party libraries
 import anime from 'animejs/lib/anime.es.js';
 
+const ZOOM_DURATION = 30000;
+const SMOKE_FAST_DURATION = 12000;
+const SMOKE_SLOW_DURATION = 26000;
+const TEXT_FADE_DURATION = 1000;
+const TEXT_GAP_DURATION = 1000;
+const TEXT_DURATION = 4000;
+const INITIAL_ZOOM = 10;
+const INITIAL_ZOOM_DELAY = (TEXT_FADE_DURATION * 2) + 3000;
+
 function start() {
     $('#stage-landscape-view').removeClass('hidden');
 
-    $('#landscape-stage-end').on('click', function () {
+    $('#landscape-view-next-stage').on('click', function () {
         $('#animation-blindfold').fadeIn(BLINDFOLD_FADE_DURATION, end);
     });
-
-    animateLandscapeView();
+    setup();
+    $('#animation-blindfold').fadeOut(
+        BLINDFOLD_FADE_DURATION * 5,
+        animateLandscapeView
+    );
 }
+
+
+function setup() {
+    var svg_container = document.getElementById('landscape-view');
+    svg_container.style.transform = `scale(${INITIAL_ZOOM})`;
+    var svg = getSvg('landscape-view');
+    animateSmoke(svg);
+}
+
 
 function animateLandscapeView() {
     if (DEBUG) {
         console.log('Landscape view loaded.');
     }
     var svg_container = document.getElementById('landscape-view');
-    var svg = getSvg('landscape-view');
-    var container_height = $('#animation-container').height();
-    var plains = svg.querySelector('#lv-plains');
-    var river = svg.querySelector('#lv-river');
-    var forest_layer_far = svg.querySelector('#lv-forest-layer-far');
-    var forest_layer_close = svg.querySelector('#lv-forest-layer-close');
-    var landscape_layers = [
-        plains,
-        river,
-        forest_layer_far,
-        forest_layer_close,
-    ];
-
-    // Move elements down
-    var layer_down_amount = container_height * .5;
-    if (DEBUG) {
-        console.log('Moving landscape layers down by: ' + layer_down_amount + 'px.');
-    }
+    var text_container = document.getElementById('landscape-ui-narrative-text');
+    var next_stage_button = document.getElementById('landscape-view-next-stage');
+    setText('It is time to return home to my whānau after a long time away...');
     anime({
-        targets: landscape_layers,
-        translateY: layer_down_amount,
-        duration: 0
-    });
-
-    var initial_delay = BLINDFOLD_FADE_DURATION * 6;
-    var text_duration = 4000;
-    var delay_duration = (initial_delay + text_duration) * .9;
-    var animation_duration = 30000;
-    var parallax_animation_duration = 0.4 * animation_duration;
-
-    $('#animation-blindfold').fadeOut(initial_delay);
-    //Show UI elements
-    displayLandscapeUi1(initial_delay, text_duration);
-
-    // Animate whole SVG upwards, with parallax effect on layers.
-    var landscape_timeline = anime.timeline({
-        duration: parallax_animation_duration,
-        easing: 'easeOutCirc',
-        delay: delay_duration,
-    });
-    landscape_timeline.add({
         targets: svg_container,
-        translateY: '-82.3%',
-        duration: animation_duration,
+        scale: [INITIAL_ZOOM, 1],
         easing: 'easeInOutSine',
-        complete: displayLandscapeUi2
-    })
-    .add({
-        targets: plains,
-        translateY: 0,
-    }, animation_duration * 0.07)
-    .add({
-        targets: river,
-        translateY: 0,
-    }, animation_duration * 0.26)
-    .add({
-        targets: forest_layer_far,
-        translateY: 0,
-    }, animation_duration * 0.42)
-    .add({
-        targets: forest_layer_close,
-        translateY: 0,
-    }, animation_duration * 0.55);
-}
-
-
-function displayLandscapeUi1(fade_out_duration, text_duration) {
-    console.log('Displaying Landscape View UI - 1.');
-    var ui_elements = document.getElementById('landscape-ui-1').children;
-    $(ui_elements).css('visibility', 'visible');
-    var ui_timeline = anime.timeline({
+        duration: ZOOM_DURATION,
+        delay: INITIAL_ZOOM_DELAY,
+    });
+    anime.timeline({
         easing: 'linear',
+        duration: TEXT_FADE_DURATION,
+    }).add({
+        // Show text "It's is time to return home..."
+        targets: text_container,
+        opacity: [0, 1],
+    }).add({
+        targets: text_container,
+        opacity: [1, 0],
+        complete: function () {
+            setText("I've been travelling for days now, but the pā is just at the end of this valley.");
+        },
+    }, `+=${TEXT_DURATION}`).add({
+        // Show text "I've been travelling..."
+        targets: text_container,
+        opacity: [0, 1],
+    }, `+=${TEXT_GAP_DURATION}`).add({
+        targets: text_container,
+        opacity: [1, 0],
+        complete: function () {
+            setText("I'll need to cross the plains...");
+        },
+    }, `+=${TEXT_DURATION}`).add({
+        // Show text "I'll need to cross..."
+        targets: text_container,
+        opacity: [0, 1],
+    }, `+=${TEXT_GAP_DURATION * 5}`).add({
+        targets: text_container,
+        opacity: [1, 0],
+        complete: function () {
+            setText("...a river...");
+        },
+    }, `+=${TEXT_DURATION * 0.8}`).add({
+        // Show text "...a river..."
+        targets: text_container,
+        opacity: [0, 1],
+    }, `+=${TEXT_GAP_DURATION}`).add({
+        targets: text_container,
+        opacity: [1, 0],
+        complete: function () {
+            setText("...but first I have to find the correct path through the forest.");
+        },
+    }, `+=${TEXT_DURATION * 0.5}`).add({
+        // Show text "...but first I have..."
+        targets: text_container,
+        opacity: [0, 1],
+    }, `+=${TEXT_GAP_DURATION}`).add({
+        targets: text_container,
+        opacity: [1, 0],
+        complete: function () {
+            setText("My whānau told me that I will come across a secret message informing me which rimu tree to look for to find the river crossing.");
+        },
+    }, `+=${TEXT_DURATION * 0.8}`).add({
+        // Show text "My whānau told me..."
+        targets: text_container,
+        opacity: [0, 1],
+    }, `+=${TEXT_GAP_DURATION}`).add({
+        targets: text_container,
+        opacity: [1, 0],
+        complete: function () {
+            setText("Not far to go now...");
+            next_stage_button.style.display = 'block';
+        },
+    }, `+=${TEXT_DURATION * 1.6}`).add({
+        // Show text "Not far to go now..."
+        targets: text_container,
+        opacity: [0, 1],
+    }, `+=${TEXT_GAP_DURATION}`).add({
+        targets: next_stage_button,
+        opacity: [0, 1],
     });
-    ui_timeline.add({
-        targets: ui_elements,
-        opacity: 1,
-        duration: 1000,
-        delay: fade_out_duration,
-        easing: 'linear'
-    })
-    .add({
-        targets: ui_elements,
-        opacity: 0,
-        duration: 1000,
-        delay: anime.stagger(750),
-        easing: 'linear'
-    }, '+=' + text_duration);
 }
 
 
-function displayLandscapeUi2() {
-    console.log('Displaying Landscape View UI - 2.');
-    var ui_elements = document.getElementById('landscape-ui-2').children;
-    $(ui_elements).css('visibility', 'visible');
+function setText(string) {
+    var text_container = document.getElementById('landscape-ui-narrative-text');
+    text_container.innerText = string;
+}
+
+
+function animateSmoke(svg) {
+    var smoke_left_fast = svg.querySelector('#lv-smoke-left-fast');
+    var smoke_right_fast = svg.querySelector('#lv-smoke-right-fast');
+    var smoke_left_slow = svg.querySelector('#lv-smoke-left-slow');
+    var smoke_right_slow = svg.querySelector('#lv-smoke-right-slow');
     anime({
-        targets: ui_elements,
-        opacity: 1,
-        duration: 1000,
-        delay: anime.stagger(750),
-        easing: 'linear'
+        targets: smoke_left_fast,
+        strokeDashoffset: [0, anime.setDashoffset],
+        easing: 'linear',
+        duration: SMOKE_FAST_DURATION,
+        loop: true
     });
+    anime({
+        targets: smoke_right_fast,
+        strokeDashoffset: [0, anime.setDashoffset],
+        easing: 'linear',
+        duration: SMOKE_FAST_DURATION,
+        loop: true
+    });
+    anime({
+        targets: smoke_left_slow,
+        strokeDashoffset: [0, anime.setDashoffset],
+        easing: 'linear',
+        duration: SMOKE_SLOW_DURATION,
+        loop: true
+    });
+    anime({
+        targets: smoke_right_slow,
+        strokeDashoffset: [0, anime.setDashoffset],
+        easing: 'linear',
+        duration: SMOKE_SLOW_DURATION,
+        loop: true
+    });
+}
+
+
+function cleanUp() {
+    document.getElementById('stage-landscape-view').innerHTML = '';
 }
 
 
 function end() {
     $('#stage-landscape-view').addClass('hidden');
+    cleanUp();
     changeStage('fern-interactive');
 }
 

8f78db04d40871fb5910eba7f9e148abbc3b2605
JackMorganNZ <jackmorgannz@gmail.com>
2020-10-07 11:26:08 +1300

    Add missing import

--- src/js/stage_before_plains.mjs
+++ src/js/stage_before_plains.mjs
@@ -1,7 +1,7 @@
 // Import modules
 import { DEBUG, BLINDFOLD_FADE_DURATION } from './constants.mjs';
 import { changeStage } from './utilities.mjs';
-import { playMusic } from './audio.mjs';
+import { playMusic, playFX } from './audio.mjs';
 
 // Import third party libraries
 import anime from 'animejs/lib/anime.es.js';
